3c8532bb0e5c18729cbc0c74bc4f60fca3f95468
marwood <marwoodcalix@gmail.com>
2025-10-07 23:26:08 +0800

    adopt change

--- backend/routes/api_router.py
+++ backend/routes/api_router.py
@@ -16,7 +16,7 @@ from entities.dto import GenCoverImgReq, AIGCTask, AIGCTaskID, GenVideoReq, Digi
     GenerateLyricsReq, GenMusicReq, BasicInfoReq, GenXAudioReq, Username1, Profile, DigitalHumanPageReq, PointsDetails
 from infra.db import aigc_task_col, aigc_task_get_by_id, aigc_task_count_by_tenant_id, digital_human_col, \
     digital_human_get_by_id, digital_human_get_by_digital_human, aigc_task_delete_by_id, digital_human_col_delete_by_id, \
-    get_profile_by_tenant_id, add_points, digital_human_save, profile_save, profiles_col
+    get_profile_by_tenant_id, add_points, digital_human_save, profile_save, profiles_col, aigc_task_save
 from infra.file import s3_upload_file
 from middleware.auth_middleware import get_optional_current_user
 from services.aigc_service import gen_cover_img_svc, gen_video_svc, aigc_task_publish_by_id, gen_lyrics_svc, \
@@ -261,16 +261,21 @@ async def adopt_digital_human(req: ID, user: Optional[dict] = Depends(get_option
     p = await get_profile_by_tenant_id(tenant_id)
     if not p or not p.verified_x_username:
         raise_error("x not verified")
-    task = await digital_human_get_by_id(req.id)
-    if p.verified_x_username != task.twitter_username:
+    human = await digital_human_get_by_id(req.id)
+    if p.verified_x_username != human.twitter_username:
         raise_error("username is not matched")
-    task.adopted = True
-    await digital_human_save(task)
+    human.adopted = True
+    human.from_tenant_id = tenant_id
+    await digital_human_save(human)
     p.adopted = True
     await profile_save(p)
 
+    task = await aigc_task_get_by_id(human.from_task_id)
+    task.tenant_id = tenant_id
+    await aigc_task_save(task)
+
     await add_points(tenant_id=tenant_id, points=100, remark="adopt_digital_human")
-    return RestResponse(data=task)
+    return RestResponse(data=human)
 
 
 @router.post("/api/x/user",
